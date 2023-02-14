a5b1968cab1b114c08351d7227fafc9a8fb93d43
Maria Eduarda <101368201+Maria22027@users.noreply.github.com>
2023-02-14 15:47:25 -0300

    Add files via upload
    C#

---
+++ caculadorabasica.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Calculadora
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bem-vindo à calculadora!");
+
+        Console.Write("Digite o primeiro número: ");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo número: ");
+        double num2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Escolha a operação (+ - * /): ");
+        char operacao = char.Parse(Console.ReadLine());
+
+        double resultado = 0.0;
+        switch (operacao)
+        {
+            case '+':
+                resultado = num1 + num2;
+                break;
+            case '-':
+                resultado = num1 - num2;
+                break;
+            case '*':
+                resultado = num1 * num2;
+                break;
+            case '/':
+                resultado = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("Operação inválida.");
+                break;
+        }
+
+        Console.WriteLine($"O resultado é: {resultado}");
+    }
+}
+
